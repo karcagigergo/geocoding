a8aabbcdd5cba713de75401903d66b6aab97c039
Gergo Karcagi <karcagigergo@gmail.com>
2021-10-05 17:17:25 -0300

    added js, to visualize map + markers on map + fitted map to markers

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -26,7 +26,5 @@ import "bootstrap";
 import { initMapbox } from '../plugins/init_mapbox';
 
 document.addEventListener('turbolinks:load', () => {
-  // Call your functions here, e.g:
-  // initSelect2();
   initMapbox();
-});
+})
