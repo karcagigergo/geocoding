2e254b5ec6deba5aec55675b646ad8b24506aa60
Gergo Karcagi <karcagigergo@gmail.com>
2021-10-08 21:51:31 -0300

    added validations to flats, made style changes on index page, added delete button on the show page

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,4 +1,7 @@
 class Flat < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+  validates :name, length: { maximum: 15,
+    too_long: "%{count} characters is the maximum allowed" }
+  validates :name, :address, presence: true, uniqueness: true
 end
